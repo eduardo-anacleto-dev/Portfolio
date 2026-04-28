c5b7d7a93b248775998106f5ff4e065d772a2025
Eduardo Souza <edu.souza.anacleto@gmail.com>
2026-04-28 16:00:41 -0300

    organizei meu script

--- site-exemplo/script.js
+++ site-exemplo/script.js
@@ -1,11 +1,9 @@
 window.alert('ola, seja bem vindo ao meu portfolio!')
-
 let cores = window.prompt(`Voce prefere um tema Claro ou Escuro?`)
 
 if (cores === 'Claro'){
     document.body.style.backgroundColor = 'white'
     document.body.style.colorr = 'black'
-    
 } else{
     document.body.style.backgroundColor = 'rgb(77, 77, 77)'
     document.body.style.color = 'white'
