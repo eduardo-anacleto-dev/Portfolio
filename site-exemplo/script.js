7e635ee8259aff500bd1a035b6c951ae0539e557
Eduardo Souza <edu.souza.anacleto@gmail.com>
2026-04-29 23:31:19 -0300

    resolvi o probelma de conflito

--- site-exemplo/script.js
+++ site-exemplo/script.js
@@ -1,23 +1,15 @@
-/*
-window.alert('ola, seja bem vindo ao meu portfolio!')
-
-
-let cores = window.prompt(`Voce prefere um tema Claro ou Escuro?`)
-
-if (cores === 'Claro'){
-    document.body.style.backgroundColor = 'white'
-    document.body.style.colorr = 'black'
-    
-} else{
-    document.body.style.backgroundColor = 'rgb(231, 210, 165)'
-    document.body.style.color = 'white'
-}
-*/
 
 const btnDark = document.querySelector('.dark')
 btnDark.addEventListener('click' , (e) => {
      document.body.style.backgroundColor = 'rgb(77,77,77)'
 })
+
+
+const btnDark = document.querySelector('.dark')
+btnDark.addEventListener('click' , (e) => {
+     document.body.style.backgroundColor = 'rgb(77,77,77)'
+})
+
 
 const btnLight = document.querySelector('.light')
 btnLight.addEventListener('click' , (e) => {
