2f1fb757580d721207f1889222a8ab75814a38f7
Eduardo Souza <edu.souza.anacleto@gmail.com>
2026-04-29 23:34:35 -0300

    removi o codigo duplicado

--- site-exemplo/script.js
+++ site-exemplo/script.js
@@ -1,10 +1,3 @@
-
-const btnDark = document.querySelector('.dark')
-btnDark.addEventListener('click' , (e) => {
-     document.body.style.backgroundColor = 'rgb(77,77,77)'
-})
-
-
 const btnDark = document.querySelector('.dark')
 btnDark.addEventListener('click' , (e) => {
      document.body.style.backgroundColor = 'rgb(77,77,77)'
