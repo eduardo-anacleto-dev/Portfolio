fa6bf2551bce3bc1fb081d276b2ae7b3014859fc
Eduardo Souza <edu.souza.anacleto@gmail.com>
2026-04-29 22:21:36 -0300

    adicionei modo dark e light

--- site-exemplo/script.js
+++ site-exemplo/script.js
@@ -1,4 +1,6 @@
+/*
 window.alert('ola, seja bem vindo ao meu portfolio!')
+
 
 let cores = window.prompt(`Voce prefere um tema Claro ou Escuro?`)
 
@@ -7,7 +9,17 @@ if (cores === 'Claro'){
     document.body.style.colorr = 'black'
     
 } else{
-    document.body.style.backgroundColor = 'rgb(77, 77, 77)'
+    document.body.style.backgroundColor = 'rgb(231, 210, 165)'
     document.body.style.color = 'white'
 }
+*/
 
+const btnDark = document.querySelector('.dark')
+btnDark.addEventListener('click' , (e) => {
+     document.body.style.backgroundColor = 'rgb(77,77,77)'
+})
+
+const btnLight = document.querySelector('.light')
+btnLight.addEventListener('click' , (e) => {
+    document.body.style.backgroundColor = 'rgb(241, 235, 220)'
+})
