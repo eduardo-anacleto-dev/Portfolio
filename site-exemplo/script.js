42e47778bd21818e1f3f19a2ae5d256e91769e8e
Eduardo Souza <edu.souza.anacleto@gmail.com>
2026-04-29 22:21:36 -0300

    adicionei modo dark e light

--- site-exemplo/script.js
+++ site-exemplo/script.js
@@ -1,10 +1,11 @@
-window.alert('ola, seja bem vindo ao meu portfolio!')
-let cores = window.prompt(`Voce prefere um tema Claro ou Escuro?`)
 
-if (cores === 'Claro'){
-    document.body.style.backgroundColor = 'white'
-    document.body.style.colorr = 'black'
-} else{
-    document.body.style.backgroundColor = 'rgb(77, 77, 77)'
-    document.body.style.color = 'white'
-}
+const btnDark = document.querySelector('.dark')
+btnDark.addEventListener('click' , (e) => {
+     document.body.style.backgroundColor = 'rgb(77,77,77)'
+})
+
+const btnLight = document.querySelector('.light')
+btnLight.addEventListener('click' , (e) => {
+    document.body.style.backgroundColor = 'rgb(241, 235, 220)'
+})
+
