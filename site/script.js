f50c1668b817d73e3553e9bb38f622d308b70b29
Eduardo Souza <edu.souza.anacleto@gmail.com>
2026-05-05 21:54:28 -0300

    mudanca de letras no clique

---
+++ site/script.js
@@ -0,0 +1,13 @@
+const btnDark = document.querySelector('.dark')
+btnDark.addEventListener('click' , (e) => {
+     document.body.style.backgroundColor = 'rgb(77,77,77)'
+     document.body.style.color = 'rgb(241, 235, 220)'
+})
+
+
+const btnLight = document.querySelector('.light')
+btnLight.addEventListener('click' , (e) => {
+    document.body.style.backgroundColor = 'rgb(241, 235, 220)'
+    document.body.style.color = 'rgb(77, 77, 77)'
+})
+
